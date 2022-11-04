8ecd0c5f1a39d09cb9c0ab0a80d143f33e8191ae
Mario Cornejo <mario.cornejo.linares@gmail.com>
2022-11-04 17:42:42 -0500

    Cambios para calcular numero de apuestas.

--- HolaMundo.cpp
+++ HolaMundo.cpp
@@ -1,6 +1,20 @@
-#include <stdio.h>
+#include <iostream>
+
+using namespace std;
 
 int main(){
-    printf("Hola Mundo\n");
+    float Monto, factor;
+    int dias;
+
+    Monto = 80;
+    factor = 1.2;
+    dias = 1;
+
+    while (Monto <= 400000)
+    {
+        Monto = Monto * factor;
+        cout << "Van #" << dias << " y el monto es : " << Monto << "\n";
+        dias++;
+    }
     return 0;
 }
