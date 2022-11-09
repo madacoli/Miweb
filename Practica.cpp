0afeb641bdedbe9c7cab0fd37b7e8458e5c5fc95
Mario Cornejo <mario.cornejo.linares@gmail.com>
2022-11-09 11:15:09 -0500

    Ejemplo1

--- Practica.cpp
+++ Practica.cpp
@@ -3,24 +3,16 @@
 
 //using namespace std;
 
+int suma(int, int);
+
 int main(){
-    float Monto, factor;
-    int dias;
-    std::string mensaje =  "Hola Mundo";
 
-    //std::cout << mensaje << std::endl;
-
-    Monto = 137;
-    factor = 1.2;
-    dias = 1;
-
-    while (Monto <= 400000)
-    {
-        Monto = Monto * factor;
-        std::cout << "Van #" << dias << " y el monto es : " << Monto << "\n";
-        dias++;
-    }
-
+    std::cout << "La suma de los numeros es : " << suma(4, 6) << std::endl;
+    std::cout << "La suma de los numeros es : " << suma(1123, 112) << std::endl;
 
     return 0;
+}
+
+int suma(int numero1, int numero2){
+    return numero1 + numero2;
 }
